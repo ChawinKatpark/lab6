ef63379b705db838f9b27f012b2356f3003bf56d
sussy <moogus@sussy.baka.ac.th>
2023-12-01 15:26:29 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 using namespace std;
 
+int x = 1;
+int y;
+int z;
+
 int main(){
+    
+    while (x != 0) {
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    
+    if (x%2 == 0) {
+        if (x != 0) {
+            y++;
+        }
+    } else {
+        z++;
+    }
+    }
+    
+    cout << "#Even numbers = " << y << "\n";
+    cout << "#Odd numbers = " << z;
     return 0;
 }
